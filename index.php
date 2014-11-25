fb419ca249ccfada2e2054256b2aa13ccbadbd2e
amaung <ayeminaung@rebbiz.com>
2014-11-25 12:53:25 +0630

    changed test namespace

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require './vendor/autoload.php';
+$loader = require './vendor/autoload.php';
+$initializer = new Devine\AutoInitializer\AutoInitializer;
 
-$initializer = new Devine\Autoinitializer\AutoInitializer;
 $test = $initializer->build("Devine\Examples\TestClass");
 var_dump($test);
